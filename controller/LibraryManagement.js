6b3e6b173093d1f02ab6fab4de227c6c4fb66625
BomScoob <sorawitkraukham@gmail.com>
2023-10-30 19:50:04 +0700

    libraryManagement find Book function

--- controller/LibraryManagement.js
+++ controller/LibraryManagement.js
@@ -4,7 +4,23 @@ function libraryManagement(){
     let books = new Array()
 
     function addBook(title, author, genre, publishedYear){
-        const newBook = new Book()
+        const newBook = new Book(title, author, genre, publishedYear)
+        books.push(newBook)
+    }
+
+    function findBooksByAuthor(authorName){
+        const bookOfAuthor = books.filter((book) => book.author === authorName)
+        return bookOfAuthor
+    }
+
+    function findBooksByGenre(genre){
+        const bookOfGenre = books.filter((book) => book.genre === genre)
+    }
+
+    return {
+        addBook,
+        findBooksByAuthor,
+        findBooksByGenre,
     }
 }
 
