2152f493bd671c1d15ff02cb1784f259e47bf09a
BomScoob <sorawitkraukham@gmail.com>
2023-10-30 19:13:12 +0700

    Genre of Book

--- model/Genre.js
+++ model/Genre.js
@@ -1,3 +1,10 @@
 const GenreType = {
-
+    Fiction:"Figtion",
+    NonFig:"Non-figtion",
+    SciFi:"Sci-fi",
+    Romance: "Romance",
+    Crime:"Crime",
+    Fantasy:"Fantasy"
 }
+
+module.exports = GenreType
